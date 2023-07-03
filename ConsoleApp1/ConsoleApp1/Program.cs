5a533fb23fa3b2207807faa5df9b2f04f2990cb4
onasheep <94965545+onasheep@users.noreply.github.com>
2023-07-03 16:00:35 +0900

    feat : git initialize

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    // {   2023, 07 , 03. Modified class position / Alpha
+
+    internal class Program
+    {   
+        
+        
+        static void Main(string[] args)
+        {
+            Ruler ruler = new Ruler(10);
+            ruler.Run();
+
+        }
+        public class Ruler
+        {
+            private const float ONE_INCH = 2.54F;
+            public int Centimeter { get; set; }
+            public float Inch
+            {
+                get { return Centimeter * ONE_INCH; }
+                private set { Centimeter = (int)(value / ONE_INCH); }
+            }
+
+            public Ruler(int cmValue)
+            {
+                Centimeter = cmValue;
+            }
+
+            public void Run()
+            {
+                Console.WriteLine($"{this.Centimeter}cm 는 {this.Inch}inch 입니다.");
+            }
+        }
+    }
+}
